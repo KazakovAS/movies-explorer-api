944f8a16bcf502889b9b95ab446a2be61bb9a8b6
Alexey Kazakov <a.kazakov@betaonline.ru>
2022-09-07 16:33:18 +0300

    fixes

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -30,7 +30,7 @@ const validateId = celebrate({
 
 const validateUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
